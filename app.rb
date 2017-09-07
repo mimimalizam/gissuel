10b7a4bf27bc978055417dedd5acca6b578423d7
Milana <milana.stojadinov@gmail.com>
2017-09-07 22:18:28 +0200

    Ask for last five issues

--- app.rb
+++ app.rb
@@ -13,14 +13,28 @@ post "/gateway" do
   repo_url = "https://api.github.com/repos/#{repo}"
 
   case action
-  when 'issues'
+  when "issues"
     resp = HTTParty.get(repo_url)
     resp = JSON.parse resp.body
     respond_message "There are #{resp['open_issues_count']} open issues on #{repo}"
+  when "last-5"
+    resp = HTTParty.get(repo_url + "/issues")
+    response = JSON.parse(resp.body)
+    last_five = response.last(5)
+    respond_message "These are last five opened issues:\n" + compose_issues(last_five)
   end
 end
 
 def respond_message message
     content_type :json
-      {:text => message}.to_json
+      { :text => message }.to_json
+end
+
+def compose_issues last_five
+  last_five.map { |issue| urlize(issue)}
+    .join("\n ")
+end
+
+def urlize(issue)
+  "     <#{issue["html_url"]}|#{issue["title"]}>"
 end

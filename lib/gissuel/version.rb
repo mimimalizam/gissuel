d01698302f7d12a2b4b9afd88c17ede7fa40c354
Milana <milana.stojadinov@gmail.com>
2018-11-25 20:12:03 +0100

    Create gem

---
+++ lib/gissuel/version.rb
@@ -0,0 +1,3 @@
+module Gissuel
+  VERSION = "0.1.0"
+end
---
+++ spec/gissuel_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Gissuel do
+  it "has a version number" do
+    expect(Gissuel::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
